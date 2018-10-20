7ec32e78aacaf7d8547d9cde31967a94347d882c
James Cho <hwisoocho@gmail.com>
2018-10-20 16:29:57 -0700

    slide logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  
+  // jQuery Fade Effects
   $("#fadeOutBtn").click(function(){
     $("#fadeBox").fadeOut(1500);
   });
@@ -8,4 +10,16 @@ $(document).ready(function(){
   $("#fadeToggleBtn").click(function(){
     $("#fadeBox").fadeToggle(1500);
   });
+
+  //jQuery Slide Effects
+  $("#slideDownBtn").click(function(){
+    $("#slideBox").slideDown(1500);
+  });
+  $("#slideUpBtn").click(function(){
+    $("#slideBox").slideUp(1500);
+  });
+  $("#slideToggleBtn").click(function(){
+    $("#slideBox").slideToggle(1500);
+  });
+
 })
